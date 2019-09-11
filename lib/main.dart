a695c064f57b347b515bec916eac1b446d28375a
Henrique Dezani <henriquedezani@Henriques-MacBook-Pro.local>
2019-09-11 11:35:24 -0300

    Versão da prova

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MovieApp());
+
+class MovieApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
